c8cb02bbf643373489585370ebec4f263e0aaaeb
Raimago <rai.arruda@hotmail.com>
2025-02-20 14:46:31 -0300

    Update script.py

--- script.py
+++ script.py
@@ -9,7 +9,7 @@ EAD_API_URL = "https://ead.conhecimentointegrado.com.br/api/doc/v1"
 
 # Autenticação Google Sheets
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-creds = ServiceAccountCredentials.from_json_keyfile_name("credenciais.json", scope)
+creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
 client = gspread.authorize(creds)
 sheet = client.open_by_key(SHEET_ID).sheet1
 
